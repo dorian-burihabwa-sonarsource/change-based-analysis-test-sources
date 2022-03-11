4451b0d4e06aecd3af7f29e980ee755d2bd8390d
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2022-03-11 15:19:07 +0100

    First commit

---
+++ src/main/java/org/example/domain/Authorization.java
@@ -0,0 +1,9 @@
+package org.example.domain;
+
+import java.util.List;
+
+public class Authorization {
+  public List<String> getAuthorizedNames() {
+    return  null;
+  }
+}
---
+++ src/main/java/org/example/domain/System.java
@@ -0,0 +1,9 @@
+package org.example.domain;
+
+public class System {
+  private int unusedField = 42;
+
+  static long getValue() {
+    return 42L;
+  }
+}
